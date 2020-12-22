854bd803004aa0b839235c15f1ba65de9d60f503
JoshBernstein <josh.bern@gmail.com>
2020-12-22 14:31:48 -0500

    Added grocery list item counter

--- Exercise_2.rb
+++ Exercise_2.rb
@@ -31,7 +31,22 @@ end
 show_list(grocery_list)
 
 # You lost count of how many things you need to pick up. Better output the total number of items on your list.
+
+puts "There are #{grocery_list.count} items on the list."
+
 # Check to see if your list includes "bananas". If it does, output "You need to pick up bananas". Otherwise, output "You don't need to pick up bananas today".
+
+# grocery_list.push("bananas")
+
+# show_list(grocery_list)
+
+# if
+#     grocery_list.include? "bananas"
+#     puts "You don't need to pick up bananas today"
+# else
+#     puts "You need to pick up bananas"
+# end
+
 # Display the second item in the list. (Don't forget that arrays indices start at zero!)
 # It turns out that everything in this grocery store you're visiting is stored alphabetically, so to better plan out what you need to buy you should sort your grocery list and output it with asterisks again.
 # After looking everywhere, you can't find the salmon. Delete it from your list and redisplay the list one last time.
