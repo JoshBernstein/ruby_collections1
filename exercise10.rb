6e0afaa3f7bf85f5469c77b2ffb140ee57e07654
JoshBernstein <josh.bern@gmail.com>
2020-12-24 13:31:02 -0500

    initial commit of exercise 2, steps 1 and 2

---
+++ exercise10.rb
@@ -0,0 +1,46 @@
+# Create a file named exercise10.rb.
+
+# Don't forget to try running your code after each step, and to commit frequently as you work!
+
+# 1. Start out by creating the following hash representing the number of students in a cohorts:
+
+# students = {
+#   :cohort1 => 34,
+#   :cohort2 => 42,
+#   :cohort3 => 22
+# }
+
+students = {
+   :cohort1 => 34,
+   :cohort2 => 42,
+   :cohort3 => 22
+}
+
+# Create a method that displays the name and number of students for each cohort, like so:
+
+    # define a method with the cohort list as an argument
+
+def cohort_size(list)
+
+    # loop through each cohort and print "There are num students in cohortx"
+
+    list.each do |name, size|
+        puts "There are #{size} students in #{name}"
+    end
+end
+
+cohort_size(students)
+
+# Add cohort 4, which had 43 students, to the hash.
+
+# Use the keys method to output all of the cohort names.
+
+# The classrooms have been expanded! Increase each cohort size by 5% and display the new results.
+
+# Delete the 2nd cohort and redisplay the hash.
+
+# BONUS: Calculate the total number of students across all cohorts using each and a variable to keep track of the total. Output the result.
+
+# BONUS: Create another similar hash called staff and display it using the same method.
+
+# After you're done, be sure you have committed and pushed everything to your Github repo.
