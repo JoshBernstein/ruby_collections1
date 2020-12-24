0f47352ead9a408292aca07567096cc114e4b560
JoshBernstein <josh.bern@gmail.com>
2020-12-24 14:48:17 -0500

    q5 answered

--- Exercise3.rb
+++ Exercise3.rb
@@ -43,6 +43,12 @@ puts students.keys
 
 # The classrooms have been expanded! Increase each cohort size by 5% and display the new results.
 
+students.each do |name, size|
+    students[name] = (size * 1.05).to_i
+end
+
+cohort_size(students)
+
 # Delete the 2nd cohort and redisplay the hash.
 
 # BONUS: Calculate the total number of students across all cohorts using each and a variable to keep track of the total. Output the result.
